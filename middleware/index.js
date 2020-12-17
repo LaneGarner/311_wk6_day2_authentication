751a13ebb4eb35cd1fe8d7a4397db2ae718417e1
Lane Garner <b.lane.garner@gmail.com>
2020-12-16 19:42:41 -0600

    updated logger function

--- middleware/index.js
+++ middleware/index.js
@@ -1,15 +1,15 @@
 const jwksRsa = require('jwks-rsa');
 const jwt = require('express-jwt');
 
-// const logger = (req, res, next) => {
-//   console.log('Logging route:', /users,  new Date().toISOString())
-//   next()
-// }
+const logger = (req, res, next) => {
+  console.log(`Logging route: ${req.path} ${new Date().toISOString()}`)
+  next()
+}
 
-const logger = (req, res, next) => {	
-  console.log(`Logged at: ${new Date()}`);	
-  next();
-};
+// const logger = (req, res, next) => {	
+//   console.log(`Logged at: ${new Date()}`);	
+//   next();
+// };
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
